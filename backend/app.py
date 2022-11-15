9ba0d479c58b82888792f085c744cd41019a4d5a
bhaskar0120 <bhaskar0120@gmail.com>
2022-11-15 21:15:42 +0530

    Fixed backend bugs

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from json import load 
+from re import sub
 
 replies = {}
 with open('langTree.json') as f:
@@ -12,9 +13,12 @@ app = Flask(__name__)
 def reply():
     global replies
     if request.content_type == "application/json":
-        check = request.json[ "sentence"]
+        check = request.json["sentence"]
         print(check)
-        chunk = sorted([i.lower() for i in check.split()])
+        check = sub("[':;-]" , '', check)
+        check = sub('[,\.\?"!]',' ',check)
+        #chunk = sorted([i.lower() for i in check.split()])
+        chunk = [i.lower() for i in check.split()]
         delim = '&&'
         x = replies
         for i in chunk:
@@ -26,13 +30,15 @@ def reply():
                         x = x[j]
                         break
                 else:
-                    return(x[delim])
+                    return(x[delim][0])
+                    print("Correct:",x[delim][1])
                     break
         else:
             while not delim in x:
                 for i in x:
                     x = x[i]
                     break
-            return(x[delim])
+            print("Correct:",x[delim][1])
+            return(x[delim][0])
     else:
         return "Unable to understand request"
--- backend/prep_data.py
+++ backend/prep_data.py
@@ -3,8 +3,10 @@ from json import dump
 
 class Sentence:
     def __init__(self, text):
-        self.text = text
-        self.sorted = sorted([i.lower() for i in text.split()])
+        text = sub("[',:;-]" , '', text)
+        self.text = sub('[\.\?"!]',' ',text)
+        self.lower = self.text.lower()
+        self.sorted = sorted([i.lower() for i in self.text.split()])
 
 def main():
     line_dict = {}
@@ -17,18 +19,27 @@ def main():
     list_of_replies = []
     with open('archive/movie_conversations.tsv') as f:
         for line in f:
+            # changes are made for debugging
+            #line = f.readline()
             list_elem = sub(' ' , ',' , line.split('\t')[-1])
             list_of_replies.append(eval('list({})'.format(list_elem)))
     replies = {}
 
+#    with open('debug.txt' , 'w', encoding="utf8") as f:
+#        for sentence in list_of_replies:
+#            for j in sentence:
+#                f.write(line_dict[j].text)
+#            f.write('\n')
+
     for sentence in list_of_replies:
         for i,curr in enumerate(sentence[:-1]):
             reply = replies
-            for k in line_dict[curr].sorted:
+            for k in line_dict[curr].lower.split():
                 if k not in reply:
                     reply[k] = {}
                 reply = reply[k]
-            reply[delim] = line_dict[sentence[i+1]].text
+            reply[delim] = (line_dict[sentence[i+1]].text,
+                    line_dict[curr].text)
 
     with open('langTree.json','w') as f:
         dump(replies,f)
